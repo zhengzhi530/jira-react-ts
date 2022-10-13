c96f824946233796428716eb00a0c0770aa5a608
zzf <1048157455@qq.com>
2022-10-13 17:28:44 +0800

    style:安装emotion,优化登录注册页面

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { loadDevTools } from "jira-dev-tool";
+import { loadServer, DevTools } from "jira-dev-tool";
+// 务必在jira-dev-tool后面引入
+import "antd/dist/antd.less";
 import { AppProviders } from "context";
 
-loadDevTools(() => {
+loadServer(() => {
   ReactDOM.render(
     <React.StrictMode>
       <AppProviders>
